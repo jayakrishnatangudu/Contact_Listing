aa0585a9d577846a37f0a9aaa09686b2c0eaf1a1
Vinay3022272 <ps3022272@kgpian.iitkgp.ac.in>
2025-10-24 21:04:19 +0530

    updated cors policy

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,8 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import userRoute from "./routes/user.route.js"
-
+import userRoute from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -11,12 +10,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://contact-listing-c8lt.vercel.app/",
+    methods: ["GET", "POST", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // route
 app.use("/api/users", userRoute);
-
 
 connectDB()
   .then(() => {
